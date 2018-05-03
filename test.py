e53dbc30a143c3868db1f393389526789fdfbc62
Alexander Kirillov <thomson77777@mail.ru>
2018-05-03 17:07:53 +0300

    add ssh key integration

--- test.py
+++ test.py
@@ -11,32 +11,28 @@ def create_droplets(do_token, build_id, ssh_keys):
             "region": "nyc3",
             "size": "s-1vcpu-1gb",
             "image": "centos-7-x64",
-            "ssh_keys": None,
+            "ssh_keys": [ssh_keys],
             "backups": False,
             "ipv6": False,
             "user_data": None,
             "private_networking": True,
-            "tags": ["web"]}
+            "tags": [build_id]}
     print(data)
     headers = {"Authorization": "Bearer {}".format(do_token)}
 
     r = requests.post(url, headers=headers, json=data)
-    print(r.text)
-    print(r.status_code)
+    return r.status_code
 
 def delete_droplets(do_token, build_id):
     url = "https://api.digitalocean.com/v2/droplets?tag_name=web"
     headers = {"Authorization": "Bearer {}".format(do_token)}
     r = requests.delete(url, headers=headers)
-    print(r.status_code)
+    return r.status_code
 
 
 if __name__ == "__main__":
     do_token = os.environ['DO_TOKEN']
     build_id = os.environ['TRAVIS_BUILD_ID']
-    ssh_keys = os.environ['SSH_KEYS']
-    print(do_token)
-    print(build_id)
-    print(ssh_keys)
-    create_droplets(do_token, build_id, ssh_keys)
-    delete_droplets(do_token, build_id)
+    ssh_keys = os.environ['DO_SSH_KEYS']
+    print(create_droplets(do_token, build_id, ssh_keys))
+    print(delete_droplets(do_token, build_id))
