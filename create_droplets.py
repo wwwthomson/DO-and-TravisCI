4825391b30e30d318f3824f96118d1b75c459093
Alexander Kirillov <thomson77777@mail.ru>
2018-05-08 15:44:40 +0300

    test wait time for vm

--- create_droplets.py
+++ create_droplets.py
@@ -48,7 +48,7 @@ def wait_status(token, tag):
             if status_droplet != "active":
                 all_status = False
         if all_status:
-            time.sleep(15) #wait loading
+            time.sleep(30) #wait loading
             break
         time.sleep(2)
 
